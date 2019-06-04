cdc97f79733a366362ff3abe41c49118da5370b8
Dmitry Fedorinov <skinklass@mail.ru>
2019-06-04 14:39:00 +0300

    initial commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Дмитрий Федоринов on 04/06/2019.
+//  Copyright © 2019 FedorinovDmitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
