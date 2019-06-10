4ed72cfc2a7c054105f9ba0b0de0026c45562081
Dmitry Fedorinov <skinklass@mail.ru>
2019-06-10 23:34:24 +0300

    In DataProvider we added check that cell has right configure for 2 section, and rework  cellForRowAt

--- ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/Controller/DataProvider.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-enum Section: Int {
+enum Section: Int, CaseIterable {
     case todo
     case done
 }
@@ -21,8 +21,7 @@ class DataProvider: NSObject {
 extension DataProvider: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 2
+        return Section.allCases.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,10 +41,18 @@ extension DataProvider: UITableViewDelegate, UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: TaskCell.self), for: indexPath) as! TaskCell
         
-        if let task = taskManager?.task(at: indexPath.row) {
-          cell.configure(withTask: task)
-        }
         
+        if let section = Section(rawValue: indexPath.section),
+            let taskManager = taskManager {
+            let task: Task
+            switch section {
+            case .todo:
+                task = taskManager.task(at: indexPath.row)
+            case .done:
+                task = taskManager.doneTask(at: indexPath.row)
+            }
+            cell.configure(withTask: task)
+        }
         return cell
     }
     
